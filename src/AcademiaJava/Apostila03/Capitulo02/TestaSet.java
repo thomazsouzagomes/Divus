7b5add6f59483c570f355e081c5b0cb4e3ec2899
Thomaz Stepheson <thomazsouzagomes@gmail.com>
2014-10-16 09:38:27 -0400

    Aula de Quarta-Feira 15/10/2014

---
+++ src/AcademiaJava/Apostila03/Capitulo02/TestaSet.java
@@ -0,0 +1,23 @@
+package AcademiaJava.Apostila03.Capitulo02;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class TestaSet {
+	public static void main(String[] args) {
+		Set set = new LinkedHashSet();
+		
+		set.add("Ana");
+		set.add("Wanessa");
+		set.add("Bruna");
+		set.add("Wanessa");
+		set.add("Ana");
+		set.add("Ana");
+		for(Object obj : set){
+			System.out.println(obj);
+		}	
+		
+	}
+
+}
