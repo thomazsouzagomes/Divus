125eb2d6d21d6380492044d79b5ad820a27fe2d8
Thomaz Stepheson <thomazsouzagomes@gmail.com>
2014-11-05 21:44:44 -0400

    03/11/2014 - Segunda-Feira - 2ª Parte

--- src/AcademiaJava/Apostila04/Capitulo02/TesteFile.java
+++ src/AcademiaJava/Apostila04/Capitulo02/TesteFile.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 
 public class TesteFile {
 	public static void main(String[] args) throws IOException {
-		File dir = new File("C:\\pastawwww");
+		File dir = new File("C:\\pasta");
 		System.out.println(dir.isDirectory()); //verifica se é diretorio 
 		System.out.println(dir.isFile()); //verifica se é diretorio arquivo
 		System.out.println(dir.canRead()); // Se tem permissão de leitura
 		System.out.println(dir.canWrite()); // Se tem permissão de escritta
 		System.out.println(dir.isHidden()); // Se é oculto
-		String[] list = dir.list(); // Lista os arquivos ou diretoria que se encontra na raiz
 		if(dir.exists()){
+			String[] list = dir.list(new FiltroPorNome(".txt"));
 			for (String aux : list){
 				System.out.println(aux);
 			}
